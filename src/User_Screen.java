7adf7afc41fa5b938b5a236e4d6f8cc6ac718bad
HaesungKang <koh1260@naver.com>
2022-11-06 23:46:05 +0900

    panel 분리.

--- src/User_Screen.java
+++ src/User_Screen.java
@@ -1,39 +1,53 @@
 import java.awt.Color;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class User_Screen extends JFrame{
 	private JPanel top;
 	private JPanel mid;
-	private JPanel bot;
+	private JPanel basket;
+	private JPanel control_panel;
+	private JButton control_btn;
 
 	
 	public User_Screen(){
 		setDisplay();
 		setBasePanel();
+
 	}
 	
 	public void setBasePanel() {
 		top = new JPanel();
 		add(top);
-		top.setVisible(true);
 		top.setLayout(null);
 		top.setBackground(Color.BLUE);
 		top.setBounds(0,0,700, 50);
 				
 		mid = new JPanel();
 		add(mid);
+		mid.setLayout(null);
 		mid.setBackground(Color.CYAN);
 		mid.setBounds(0, 50, 700, 400);
 		
-		bot = new JPanel();
-		add(bot);
-		bot.setBackground(Color.GRAY);
-		mid.setBounds(0, 450, 700, 450);
-	}
-	public void setSubPanel() {
+		basket = new JPanel();
+		add(basket);
+		basket.setLayout(new GridLayout(1,7));
+		basket.setBackground(Color.ORANGE);
+		basket.setBounds(0, 450, 515, 450);
 		
+		control_panel = new JPanel();
+		add(control_panel);
+		control_panel.setLayout(null);
+		control_panel.setBackground(Color.GREEN);
+		control_panel.setBounds(515, 450, 185, 450);
+
+	}
+	
+	public void setSubPanel() {
+
 	}
 
 	public void setDisplay() {		
