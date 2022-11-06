17816ae2d89f81906a6411c36a8d4b5a5cf6c6b9
HaesungKang <koh1260@naver.com>
2022-11-07 01:52:28 +0900

    frame 구성 다시함.

--- src/User_Screen.java
+++ src/User_Screen.java
@@ -1,53 +1,82 @@
 import java.awt.Color;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class User_Screen extends JFrame{
-	private JPanel top;
-	private JPanel mid;
+	int num = 1;
+	private JPanel menu_panel;
 	private JPanel basket;
 	private JPanel control_panel;
-	private JButton control_btn;
-
+	
+	private JLabel count;
+	private JButton init_btn;
+	private JButton pay_btn;
+	
+	private JButton[] menus;
 	
 	public User_Screen(){
 		setDisplay();
-		setBasePanel();
-
+		setPanel();
+		setControlPanel();
+		menuBtn();
 	}
-	
-	public void setBasePanel() {
-		top = new JPanel();
-		add(top);
-		top.setLayout(null);
-		top.setBackground(Color.BLUE);
-		top.setBounds(0,0,700, 50);
-				
-		mid = new JPanel();
-		add(mid);
-		mid.setLayout(null);
-		mid.setBackground(Color.CYAN);
-		mid.setBounds(0, 50, 700, 400);
-		
+
+	public void setPanel() {
+		menu_panel = new JPanel();
+		add(menu_panel);
+
+		menu_panel.setBounds(43,50,600, 400);
+//		menu_panel.setBackground(Color.LIGHT_GRAY);
+		menu_panel.setLayout(new GridLayout(2,3,40,40));
+
 		basket = new JPanel();
 		add(basket);
-		basket.setLayout(new GridLayout(1,7));
-		basket.setBackground(Color.ORANGE);
-		basket.setBounds(0, 450, 515, 450);
+		basket.setBounds(0,500, 500, 400 );
+		basket.setBackground(Color.gray);
+		basket.setLayout(new BoxLayout(basket, BoxLayout.Y_AXIS));
 		
 		control_panel = new JPanel();
 		add(control_panel);
+		control_panel.setBounds(500,500, 200, 400 );
 		control_panel.setLayout(null);
-		control_panel.setBackground(Color.GREEN);
-		control_panel.setBounds(515, 450, 185, 450);
-
+	}
+	public void menuBtn() {
+		menus = new JButton[6];
+		
+		for(int i = 0; i < 6; i++) {
+			menus[i] = new JButton("menu" + Integer.toString(i));
+			menus[i].setSize(30,30);
+			menu_panel.add(menus[i]);
+		}
 	}
 	
-	public void setSubPanel() {
-
+	public void setControlPanel() {
+		init_btn = new JButton("초기화");
+		control_panel.add(init_btn);
+		init_btn.setBounds(0,0,190, 75);
+		init_btn.setBorderPainted(false);
+		init_btn.setFocusPainted(false);	
+		
+		count = new JLabel("총량  " + Integer.toString(num));
+		control_panel.add(count);
+		count.setBounds(0,75,190, 75);
+		count.setBackground(Color.WHITE);
+		count.setHorizontalAlignment(JLabel.CENTER);
+		
+		pay_btn = new JButton("결제");
+		control_panel.add(pay_btn);
+		pay_btn.setBounds(0,150,190, 220);
+		pay_btn.setBorderPainted(false);
+		pay_btn.setFocusPainted(false);	
+	}
+	public void setBasket() {
+		
 	}
 
 	public void setDisplay() {		
