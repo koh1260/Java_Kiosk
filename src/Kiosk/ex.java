4793aef8378a528433b70ee3af0e8192fca508e7
HaesungKang <koh1260@naver.com>
2022-11-28 05:34:47 +0900

    ip, port, pw

--- src/Kiosk/ex.java
+++ src/Kiosk/ex.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 
 public class ex {
 	
-	public  static String db_url = "jdbc:mysql://113.198.233.244:13306/java_kiosk";
+	public  static String db_url = "jdbc:mysql://localhost:*****/java_kiosk";
 	public static String db_user = "root";
-	public static String db_pw = "1306";
+	public static String db_pw = "****";
 
 	
 	public static void main(String[] args) {
