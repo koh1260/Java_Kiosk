426e5bd117ca0f07438e7adf5c426642e6240629
HaesungKang <koh1260@naver.com>
2022-11-29 06:07:56 +0900

    .

--- src/Kiosk/ex.java
+++ src/Kiosk/ex.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 
 public class ex {
 	public static int stan_number = 1;
-	public  static String db_url = "jdbc:mysql://113.198.233.244:13306/java_kiosk";
+	public  static String db_url = "jdbc:mysql://localhost:13306/java_kiosk";
 	public static String db_user = "root";
 	public static String db_pw = "1306";
 	public static int number = 0;
